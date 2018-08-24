85719eceb94ac334d909983a33da35b4b3db4129
Tarlles Roman <40526902+TarllesRoman@users.noreply.github.com>
2018-08-24 10:59:00 -0300

    ..

---
+++ WSConsumer/CEPConsumer.java
@@ -0,0 +1,34 @@
+package ifbarbacena.taroman.ws.ex2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class CEPConsumer {
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("https://viacep.com.br/ws/36200074/json");
+			
+			URLConnection con = url.openConnection();
+			
+			BufferedReader input = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+			
+			String line;
+			StringBuilder source = new StringBuilder();
+			
+			while((line = input.readLine()) != null)
+				source.append(line);
+			
+			input.close();
+			
+			System.out.println(source.toString());
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
