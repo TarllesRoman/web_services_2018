2ee3481d4edf52ed6bbf1017b788fea6c4deb8b2
Tarlles Roman <40526902+TarllesRoman@users.noreply.github.com>
2018-10-05 15:29:54 -0300

    minha primeira api

---
+++ Flask/API.py
@@ -0,0 +1,69 @@
+import json
+import os
+import request
+
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/arquivos', methods=['GET'])
+def arquivos():
+    return json.dumps({'GET /arquivos/json':'Retorna um json com o nome de todos os arquivos',
+            'GET /arquivos/xml':'Retorna um xml com o nome de todos os arquivos',
+            'GET /arquivos/nome_arquivo':'Retorna o conteudo textual do arquivo',
+            'DELETE /arquivos':'Remove todos os arquivos',
+            'DELETE /arquivos/nome_arquivo':'Remove o arquivo com nomeado como: nome_arquivo',
+            'PUT /arquivos/nome_arquivo':'Cria um arquivo com esse nome e conteudo, caso ja exista atualiza seu conteudo'})
+
+@app.route('/arquivos/json', methods=['GET'])
+def arquivos_json():
+    list_files = os.listdir('./Arquivos')
+    return json.dumps({'Arquivos':list_files})
+
+@app.route('/arquivos/xml', methods=['GET'])
+def arquivos_xml():
+
+    return json.dumps({'cod':404,'msg':'Implementacao nao encontrada'})
+
+@app.route('/arquivos/<arquivo>', methods=['GET'])
+def arquivos_get(arquivo):
+    try:
+        arq = open('./Arquivos/'+arquivo, 'r')
+        content = arq.read()
+        arq.close()
+        return content
+    except:
+        return json.dumps({'cod':404, 'msg':'Arquivo nao encontrado'})
+
+@app.route('/arquivos', methods=['DELETE'])
+def arquivos_deletall():
+    os.removedirs('Arquivos')
+    os.mkdir('Arquivos')
+    return json.dumps({'cod':200,'msg':'Todos os arquivos foram deletados'})
+
+@app.route('/arquivos/<arquivo>', methods=['DELETE'])
+def arquivos_delete(arquivo):
+    try:
+        os.remove('./Arquivos/'+arquivo)
+        return json.dumps({'cod':200,'msg':'Arquivo '+arquivo+' deletado'})
+    except:
+        return json.dumps({'cod':404, 'msg':'Arquivo nao encontrado'})
+
+
+
+@app.route('/arquivos/<arquivo>', methods=['PUT'])
+def arquivos_put(arquivo):
+    try:
+        arq = open('./Arquivos/'+arquivo, 'w')
+        content = request.form['data']
+        arq.write(content)
+        arq.close()
+        return json.dumps({'cod':200, 'msg':'Arquivo criado com sucesso'})
+    except:
+        return json.dumps({'cod':400, 'msg':'Falha ao criar o arquivo'})
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
